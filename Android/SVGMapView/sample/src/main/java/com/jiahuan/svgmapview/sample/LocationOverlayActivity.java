2df897b370d41e93c4234f70d0e2a12e069af436
andrefonsecacc <andre.j.fonseca@gmail.com>
2017-05-14 18:55:08 +0100

    add test to map

--- Android/SVGMapView/sample/src/main/java/com/jiahuan/svgmapview/sample/LocationOverlayActivity.java
+++ Android/SVGMapView/sample/src/main/java/com/jiahuan/svgmapview/sample/LocationOverlayActivity.java
@@ -1,5 +1,6 @@
 package com.jiahuan.svgmapview.sample;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PointF;
@@ -15,11 +16,17 @@ import com.jiahuan.svgmapview.core.helper.map.SVGBuilder;
 import com.jiahuan.svgmapview.overlay.SVGMapLocationOverlay;
 import com.jiahuan.svgmapview.sample.helper.AssetsHelper;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class LocationOverlayActivity extends ActionBarActivity
 {
     private SVGMapView mapView;
-
+    private int LIMIT_INICIAL_X=85;
+    private int LIMIT_FINAL_X=630;
+    private SVGMapLocationOverlay locationOverlay;
+    private final int FIVE_SECONDS = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,15 +37,13 @@ public class LocationOverlayActivity extends ActionBarActivity
 
         mapView = (SVGMapView) findViewById(R.id.location_mapview);
 
-        mapView.registerMapViewListener(new SVGMapViewListener()
-        {
+        mapView.registerMapViewListener(new SVGMapViewListener() {
             @Override
-            public void onMapLoadComplete()
-            {
-                SVGMapLocationOverlay locationOverlay = new SVGMapLocationOverlay(mapView);
+            public void onMapLoadComplete() {
+                locationOverlay = new SVGMapLocationOverlay(mapView);
 
                 locationOverlay.setIndicatorArrowBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.indicator_arrow));
-                locationOverlay.setPosition(new PointF(630, 350));
+                locationOverlay.setPosition(new PointF(LIMIT_INICIAL_X, 350));
                 locationOverlay.setIndicatorCircleRotateDegree(90);
                 locationOverlay.setMode(SVGMapLocationOverlay.MODE_COMPASS);
                 locationOverlay.setIndicatorArrowRotateDegree(-45);
@@ -57,5 +62,43 @@ public class LocationOverlayActivity extends ActionBarActivity
             }
         });
         mapView.loadMap(AssetsHelper.getContent(this, "dcc-piso1-cortado.svg"));
+        Timer timer = new Timer();
+
+        TimerTask timerTask = new TimerTask() {
+            int i=0;
+            @Override
+            public void run() {
+
+                testpath(i);
+                if(LIMIT_INICIAL_X+i<LIMIT_FINAL_X){i=i+5;}
+
+            }
+        };
+
+
+    timer.schedule(timerTask, 0, 1000);
     }
+
+
+
+    private void testpath(int i) {
+        Log.i("path","entrei aqui!");
+        Log.i("path",String.valueOf(i));
+        mapView.getOverLays().remove(locationOverlay);
+        //mapView.getOverLays().
+        locationOverlay = new SVGMapLocationOverlay(mapView);
+        locationOverlay.setIndicatorArrowBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.indicator_arrow));
+        locationOverlay.setPosition(new PointF(LIMIT_INICIAL_X + i, 350));
+        locationOverlay.setIndicatorCircleRotateDegree(90);
+        locationOverlay.setMode(SVGMapLocationOverlay.MODE_COMPASS);
+        locationOverlay.setIndicatorArrowRotateDegree(-45);
+        //
+        mapView.getOverLays().add(locationOverlay);
+
+        mapView.refresh();
+       //mapView.loadMap(AssetsHelper.getContent(this, "dcc-piso1-cortado.svg"));
+
+
+    }
+
 }
